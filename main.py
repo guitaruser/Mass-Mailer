1ee1898ee7efe63ab5dc140f15da054fa63c2012
Tarun KS <65398433+guitaruser@users.noreply.github.com>
2021-09-30 13:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import re
 fp = open('message.txt', 'rb')
 msg = MIMEText(fp.read())
 fp.close()
-msg['Subject'] = 'Subject goes here'
-msg['From'] = 'sender.address@gmail.com'
+msg['Subject'] = 'SUBJECT HERE'
+msg['From'] = 'YOUR_EMAIL@gmail.com'
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
-server.login('sender.address@gmail.com', 'password')
+server.login('YOUR_EMAIL@gmail.com', 'YOUR PASSWORD')
 email_data = csv.reader(open('email.csv', 'rb'))
 email_pattern= re.compile("^.+@.+\..+$")
 for row in email_data:
